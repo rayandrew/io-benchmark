6c474c16723d709aface43310b43d661fe747398
Ray Andrew <raydreww@gmail.com>
2026-03-31 20:37:55 -0700

    feat(ior): update job name format and add new scripts with aggregation rules

--- ior/run-dft-agg-selective.sh
+++ ior/run-dft-agg-selective.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 SOURCE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-export APP_ID=dft-agg-full
+export APP_ID=dft-agg-selective
 export JOB_NAME="ior-$APP_ID"
 export DFTRACER_ENABLE=1
 export DFTRACER_ENABLE_AGGREGATION=1
-export DFTRACER_AGGREGATION_TYPE=FULL
+export DFTRACER_AGGREGATION_TYPE=SELECTIVE
+export DFTRACER_AGGREGATION_FILE="/usr/workspace/sinurat1/io-benchmark/ior/agg-rules.yaml"
 export DFTRACER_TRACE_INTERVAL_MS=5000
 
 bash "${SOURCE_DIR}/run.sh"
